744d2214b235e63d2497697645048b9e6e18e4f6
Crowdbotics <team@crowdbotics.com>
2024-02-05 12:30:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "autumn-dust-45870",
+  url: "https://autumn-dust-45870.botics.co",
+  api: "https://autumn-dust-45870.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
